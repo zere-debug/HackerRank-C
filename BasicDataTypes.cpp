4846ca176babe4feda98b59ed7ba799fafbf9441
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-04 21:26:35 +0600

    Issue02

---
+++ BasicDataTypes.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+ int main() {
+     int a;
+     long b;
+     char c;
+     float d;
+     double e;
+
+     cin >> a >> b >> c >> d;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout.precision(3);
+
+    cout << d << endl;
+    cout.precision(9);
+
+    cout << e << endl;
+    return 0;
+}
