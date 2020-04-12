18812febb68033ec8446ea17f461a98645e91aae
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-12 21:27:46 +0600

    Issue11

---
+++ Strings.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+
+    cin >> a;
+    cin >> b;
+
+    cout << a.size() << " " << b.size();
+    cout << endl;
+
+    cout << a + b;
+    cout << endl;
+
+    string d = a;
+    a[0] = b[0];
+    b[0] = d[0];
+    cout << a << " " << b;
+
+    return 0;
+}
