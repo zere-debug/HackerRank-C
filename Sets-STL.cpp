6522c9f0559f850a894f1425b1d8ee8668e18e7c
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-13 19:41:47 +0600

    Issue12

---
+++ Sets-STL.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+	int iCount;
+	cin >> iCount;
+
+	set<int> ss;
+	for (int i = 0; i < iCount; ++i) {
+		int type, query;
+		cin >> type >> query;
+
+		switch (type) {
+		case 1: ss.insert(query); break;
+		case 2: ss.erase(query); break;
+		case 3: cout << (ss.find(query) == ss.end() ? "No" : "Yes") << endl;
+
+		}
+	}
+	return 0;
+}
+
+
