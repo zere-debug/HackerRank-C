2dfd3cc203aaca26d7ea4e05bb34148a79575384
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-14 21:11:50 +0600

    Issue13

---
+++ Maps-STL.cpp
@@ -0,0 +1,32 @@
+﻿
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int q;
+    cin >> q;
+    string name;
+    map<string, int> notes;
+    for (int i = 0; i < q; ++i)
+    {
+        int type = 0, note = 0;
+        cin >> type >> name;
+        switch (type)
+        {
+        case 1:
+            cin >> note;
+            notes[name] += note; break;
+        case 2:
+            notes.erase(name); break;
+        case 3:
+            cout << notes[name] << endl; break;
+        }
+    } return 0;
+}
