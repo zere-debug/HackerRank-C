f34d064035c284214e5a075fb27711988aeb820e
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-24 22:38:46 +0600

    Issue20

---
+++ InheritanceIntroduction.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Triangle {
+public:
+	void triangle() {
+		cout << "I am a triangle" << endl;
+	}
+};
+
+class Isosceles : public Triangle {
+public:
+	void isosceles() {
+		cout << "I am an isosceles triangle" << endl;
+	}
+
+	void description() {
+		cout << "An isoscele triangle has two equal sides" << endl;
+	}
+};
+
+int main() {
+	Isosceles isc;
+	isc.isosceles();
+	isc.description();
+	isc.triangle();
+
+	return 0;
+}
